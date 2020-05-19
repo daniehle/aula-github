03ed931e501be5b17ce017bd1b992532864d08ba
Daniele Silva <daniehle@gmail.com>
2020-05-19 11:43:01 -0300

    Projeto criado

---
+++ src/snippet/Snippet.java
@@ -0,0 +1,15 @@
+package snippet;
+
+public class Snippet {
+	Ampla experiência em desenvolvimento Java e que saiba desenhar suas aplicações a partir de 
+	testes utilizando técnicas como TDD. 
+	React, Angular, Node, Spring Stack API REST 
+	Java 8+, Spring Stack, JavaEE, Webservices Rest e Soap. 
+	Docker, GIT, GITFLOW, GITLAB, Jenkins 
+	Jboss 
+	JMS 
+	Oracle, MongoDB, Cassandra, DEVops 
+	Arquitetura voltada a eventos e processamento distribuído. 
+	 
+}
+
